aa8218c34bd471fb3c03c5847e050d4a1790860d
Jack Tickoo <2397224T@student.gla.ac.uk>
2020-02-04 09:04:00 +0000

    **FIXED BUG**

--- rango/forms.py
+++ rango/forms.py
@@ -3,8 +3,8 @@ from rango.models import Page, Category
 
 class CategoryForm(forms.ModelForm):
 	name  = forms.CharField(max_length=128, help_text="Please enter the category name.")
-	views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-	likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+	views = forms.IntegerField(widget=forms.HiddenInput(), required=False, initial=0)
+	likes = forms.IntegerField(widget=forms.HiddenInput(), required=False, initial=0)
 	slug  = forms.CharField(widget=forms.HiddenInput(), required=False)
 
 	class Meta:
