237669d34de4e5da070a4b0a945dcae412c1e4bc
Jack Tickoo <2397224T@student.gla.ac.uk>
2020-02-11 15:20:13 +0000

    Chapter 9 completed, untested

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -27,6 +27,8 @@ MEDIA_ROOT = MEDIA_DIR
 
 MEDIA_URL = '/media/'
 
+LOGIN_URL = 'rango:login'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
